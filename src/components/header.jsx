3f22ef97f19c92869fb0aca604d87361018f1deb
Tracy-miranja <barazatracy44@gmail.com>
2024-12-20 14:09:05 +0300

    Modify about

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import logo from "../assets/amsol-hr-outsourcing.png"
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -34,7 +35,7 @@ function Header() {
       <div className="container mx-auto flex items-center justify-around p-3">
         {/* Logo Section */}
         <div className="text-xl font-bold">
-          AMSOL HR
+         <img src={logo} className='h-[5vh]'/>
         </div>
 
         {/* Navigation Links (Desktop) */}
@@ -62,9 +63,9 @@ function Header() {
             </button>
             {isProductOpen && (
               <div className="absolute top-8 left-1/2 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[50vw]">
-                <a href="#product1" className="block text-black py-2 hover:text-blue-300">Product 1</a>
-                <a href="#product2" className="block text-black py-2 hover:text-blue-300">Product 2</a>
-                <a href="#product3" className="block text-black py-2 hover:text-blue-300">Product 3</a>
+                <a href="#product1" className="block text-black py-2 hover:text-blue-300">HRMS</a>
+                <a href="#product2" className="block text-black py-2 hover:text-blue-300">CMS</a>
+                <a href="#product3" className="block text-black py-2 hover:text-blue-300">ATS</a>
               </div>
             )}
           </div>
@@ -91,9 +92,19 @@ function Header() {
               </svg>
             </button>
             {isAboutOpen && (
-              <div className="absolute top-8 left-1/2 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[50vw]">
+              <div className="grid absolute top-8 left-1/2 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[50vw]">
+                <div>
+                <div className='h-[10vh] text-black'>hello</div>
                 <a href="#about1" className="block text-black py-2 hover:text-blue-300">About Us</a>
+                </div>
+                <div>
+                <div className='h-[10vh] text-black'>hello</div>
+                    <a href="#team" className="block text-black py-2 hover:text-blue-300">Our Team</a>
+                    </div>
+                <div>
+                <div className='h-[10vh] text-black'>hello</div>
                 <a href="#team" className="block text-black py-2 hover:text-blue-300">Our Team</a>
+                </div>          
               </div>
             )}
           </div>
@@ -122,7 +133,7 @@ function Header() {
               </svg>
             </button>
             {isGlobalOpen && (
-              <div className="absolute top-8 left-1/2 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[10vw]">
+              <div className="absolute top-8 left-1/4 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[10vw]">
                 <a href="#global1" className="block text-black py-2 hover:text-blue-300">Kenya</a>
                 <a href="#global2" className="block text-black py-2 hover:text-blue-300">Uganda</a>
                 <a href="#global2" className="block text-black py-2 hover:text-blue-300">Tanzania</a>
@@ -130,18 +141,14 @@ function Header() {
               </div>
             )}
           </div>
-
-
         </nav>
-
         <div className="flex text-md font-bold gap-4 text-center items-center">
           <button className="bg-green-400 p-1 pl-2 pr-2 hover:text-blue-300 hover:bg-white border border-blue-400 rounded-full">
             Login
           </button>
           <button className="bg-green-400 p-1 pl-2 pr-2  hover:text-blue-300 hover:bg-white border border-blue-400 rounded-full">
             Request Demo
-          </button>
-          
+          </button> 
         </div>
 
         {/* Mobile Menu Button */}
@@ -169,30 +176,115 @@ function Header() {
       {/* Mobile Navigation Menu */}
       {isMenuOpen && (
         <nav className="md:hidden bg-blue-800 p-4 h-screen">
-          <a
-            href="#home"
-            className="block text-white hover:text-blue-300 py-2"
-          >
-            Home
-          </a>
+          <div className="relative" ref={productDropdownRef}>
+            <button
+              onClick={() => setIsProductOpen(!isProductOpen)}
+              className="flex items-center space-x-2 hover:text-blue-300"
+            >
+              <span>Product</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
+            {isProductOpen && (
+              <div className="absolute top-8 left-1/2 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[50vw]">
+                <a href="#product1" className="block text-black py-2 hover:text-blue-300">HRMS</a>
+                <a href="#product2" className="block text-black py-2 hover:text-blue-300">CMS</a>
+                <a href="#product3" className="block text-black py-2 hover:text-blue-300">ATS</a>
+              </div>
+            )}
+          </div>
           <a
             href="#services"
             className="block text-white hover:text-blue-300 py-2"
           >
             Services
           </a>
-          <a
-            href="#about"
-            className="block text-white hover:text-blue-300 py-2"
-          >
-            About
-          </a>
+          <div className="relative" ref={aboutDropdownRef}>
+            <button
+              onClick={() => setIsAboutOpen(!isAboutOpen)}
+              className="flex items-center space-x-2 hover:text-blue-300"
+            >
+              <span>About</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
+            {isAboutOpen && (
+              <div className="grid absolute top-8 left-1/2 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[50vw]">
+                <div>
+                <div className='h-[10vh] text-black'>hello</div>
+                <a href="#about1" className="block text-black py-2 hover:text-blue-300">About Us</a>
+                </div>
+                <div>
+                <div className='h-[10vh] text-black'>hello</div>
+                    <a href="#team" className="block text-black py-2 hover:text-blue-300">Our Team</a>
+                    </div>
+                <div>
+                <div className='h-[10vh] text-black'>hello</div>
+                <a href="#team" className="block text-black py-2 hover:text-blue-300">Our Team</a>
+                </div>          
+              </div>
+            )}
+          </div>
           <a
             href="#contact"
             className="block text-white hover:text-blue-300 py-2"
           >
             Contact
           </a>
+          <div className="relative" ref={globalDropdownRef}>
+            <button
+              onClick={() => setIsGlobalOpen(!isGlobalOpen)}
+              className="flex items-center space-x-2 hover:text-blue-300"
+            >
+              <span>Global</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
+            {isGlobalOpen && (
+              <div className="absolute top-8 left-1/4 transform -translate-x-1/2 bg-white shadow-md p-2 rounded-md w-[10vw]">
+                <a href="#global1" className="block text-black py-2 hover:text-blue-300">Kenya</a>
+                <a href="#global2" className="block text-black py-2 hover:text-blue-300">Uganda</a>
+                <a href="#global2" className="block text-black py-2 hover:text-blue-300">Tanzania</a>
+                <a href="#global2" className="block text-black py-2 hover:text-blue-300">Rwanda</a>
+              </div>
+            )}
+          </div>
         </nav>
       )}
     </header>
